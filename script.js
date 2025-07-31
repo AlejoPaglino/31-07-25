9eb6de49ffec0b96200037fdc190204cc9255f72
alumno <alumno@Instituto.local>
2025-07-31 11:17:42 -0300

    31/07/25

---
+++ script.js
@@ -0,0 +1,35 @@
+let estudiantes =[{
+nombre : 'Alejo',
+apellido : 'Paglino',
+fechaNacimiento : '29 de Agosto de 2008',
+NotaExamen : 10
+},
+{
+    nombre : 'Luciano',
+    apellido : 'Ciaffoni',
+    fechaNacimiento : '14 de julio de 2008',
+    NotaExamen : 8
+    },
+    {
+        nombre : 'Agustin',
+        apellido : 'Kramer',
+        fechaNacimiento : '18 de julio de 2008',
+        NotaExamen : 9
+        },
+        {
+            nombre : 'Benjamin',
+            apellido : 'Reinhardt',
+            fechaNacimiento : '5 de diciembre de 2008',
+            NotaExamen : 6
+            },
+            {
+                nombre : 'Facundo',
+                apellido : 'Pestana',
+                fechaNacimiento : '30 de diciembre de 2008',
+                NotaExamen : 7
+                }
+]
+for (let indice = 0; indice < estudiantes.length; indice++) {
+    const element = estudiantes[indice].estudiantes.nombre +' te sacaste un ' + estudiantes.NotaExamen;
+    
+}
