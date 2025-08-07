466844fe12bc0fa26ddbbb3af587095c8b1b1d01
alumno <alumno@Instituto.local>
2025-08-07 11:23:26 -0300

    07/08/25

---
+++ 07/08/25/script2.js
@@ -0,0 +1,27 @@
+let notas =[6,7,8,9,6,7,8,9,6,7,8,9,6,]
+let cantidadNotas = notas.length
+
+let sumaNotas = 0
+for (let i = 0; i < notas.length; i++) {
+    const element = array[index];
+  //  sumaNotas += notas[i]
+sumaNotas = sumaNotas + notas[i]
+}
+
+console.log(sumaNotas / cantidadNotas)
+
+let i = 2
+while (i<=10) {
+    console.log(i)
+    i = i + 2
+}
+
+
+
+// 4 
+
+let numeros = [10,6,77,2,5]
+
+for (let i = 0; i < numeros.length; i++) {
+    numeros[i] = numeros[i] * numeros[i]
+}
